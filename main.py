d7bad69ae9786baa091313044055d6f2ee73573d
Paul Marsh <61422343+PTMGitHub@users.noreply.github.com>
2022-01-09 13:58:44 +0000

    adds templates

--- main.py
+++ main.py
@@ -4,20 +4,25 @@
 from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 from __init__ import create_app
+
 ####################################################################
 # our main blueprint
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 ####################################################################
-@main.route('/') # home page that return 'index'
+@main.route("/")  # home page that return 'index'
 def index():
-  return 'index'
+    return render_template("index.html")
+
+
 ####################################################################
-@main.route('/profile') # profile page that return 'profile'
+@main.route("/profile")  # profile page that return 'profile'
 def profile():
-  return 'profile'
+    return render_template("profile.html")
+
+
 ####################################################################
-app = create_app() # we initialize our flask app using the
-#__init__.py function
+app = create_app()  # we initialize our flask app using the
+# __init__.py function
 ####################################################################
-if __name__ == '__main__':
-  app.run(debug=True) # run the flask app on debug mode
+if __name__ == "__main__":
+    app.run(debug=True)  # run the flask app on debug mode
