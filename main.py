89673b59e4d94ade58e079dc124e8555c7a98734
Paul Marsh <61422343+PTMGitHub@users.noreply.github.com>
2022-01-09 15:00:43 +0000

    edits so signup, login, profile and logout now works

--- main.py
+++ main.py
@@ -3,22 +3,21 @@
 ####################################################################
 from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
-from __init__ import create_app
+from __init__ import create_app, db
 
 ####################################################################
 # our main blueprint
 main = Blueprint("main", __name__)
 ####################################################################
-@main.route('/profile') # profile page that return 'profile'
-@login_required
-def profile():
-    return render_template('profile.html', name=current_user.name)
+@main.route('/') # home page that return 'index'
+def index():
+    return render_template('index.html')
 
 
 ####################################################################
 @main.route("/profile")  # profile page that return 'profile'
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", name=current_user.name)
 
 
 ####################################################################
@@ -26,5 +25,5 @@ app = create_app()  # we initialize our flask app using the
 # __init__.py function
 ####################################################################
 if __name__ == "__main__":
-    db.create_all(app=create_app()) # create the SQLite database
+    db.create_all(app=create_app())  # create the SQLite database
     app.run(debug=True)  # run the flask app on debug mode
