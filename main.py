e44b69245839e54a5debaf691d996476151acf17
Paul Marsh <61422343+PTMGitHub@users.noreply.github.com>
2022-01-09 14:37:07 +0000

    adds signup, login and logout code

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ from __init__ import create_app
 # our main blueprint
 main = Blueprint("main", __name__)
 ####################################################################
-@main.route("/")  # home page that return 'index'
-def index():
-    return render_template("index.html")
+@main.route('/profile') # profile page that return 'profile'
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
 
 
 ####################################################################
@@ -25,4 +26,5 @@ app = create_app()  # we initialize our flask app using the
 # __init__.py function
 ####################################################################
 if __name__ == "__main__":
+    db.create_all(app=create_app()) # create the SQLite database
     app.run(debug=True)  # run the flask app on debug mode
